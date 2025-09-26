d207ff185deb6f19893ca15d464fd367b37f11cd
Alpha Sylla <alpsy04@gmail.com>
2025-09-26 21:56:21 +0200

    fixed URL of API

--- pages/2_Model_Information.py
+++ pages/2_Model_Information.py
@@ -35,7 +35,7 @@ st.markdown("""
 #  streamlit "st.session_state" variable initialisation    
 
 if 'api_root' not in st.session_state:
-    st.session_state['api_root'] = 'https://stratview-api-0373e4642705.herokuapp.com/isapp/v1/resources'
+    st.session_state['api_root'] = 'https://stratview-0a6cd8d77f03.herokuapp.com/isapp/v1/resources'
 
 if 'persist' not in st.session_state:
     st.session_state['persist'] = 0
